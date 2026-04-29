56d07b984f4f97ee0787f00d2880471a9de01f10
Muhammet Enes BIÇAK <enessbicak@hotmail.com>
2026-04-29 12:08:35 +0300

    Refactor ALU control logic for clarity and correctness

--- aludec.sv
+++ aludec.sv
@@ -1,4 +1,4 @@
-`timescale 1ns/1ps
+				  
 
 module aludec(input  logic       opb5,
               input  logic [2:0] funct3,
@@ -11,17 +11,17 @@ module aludec(input  logic       opb5,
 
   always_comb
     case(ALUOp)
-      2'b00:                ALUControl = 3'b010; // Toplama (lw, sw vb.) - TV'ye göre
-      2'b01:                ALUControl = 3'b110; // Çıkarma (beq) - TV'ye göre
-      default: case(funct3)                      // R-type veya I-type ALU
+      2'b00:                ALUControl = 3'b000; // Toplama (add)
+      2'b01:                ALUControl = 3'b001; // Çıkarma (sub)
+      default: case(funct3)
                  3'b000:  if (RtypeSub)
-                            ALUControl = 3'b110; // sub
+                            ALUControl = 3'b001; // sub
                           else
-                            ALUControl = 3'b010; // add, addi
-                 3'b010:  ALUControl = 3'b111; // slt, slti
-                 3'b110:  ALUControl = 3'b001; // or, ori
-                 3'b111:  ALUControl = 3'b000; // and, andi
-                 default: ALUControl = 3'b000; // Varsayılan
+                            ALUControl = 3'b000; // add, addi
+                 3'b010:  ALUControl = 3'b101; // slt, slti
+                 3'b110:  ALUControl = 3'b011; // or, ori
+                 3'b111:  ALUControl = 3'b010; // and, andi
+                 default: ALUControl = 3'b000;
                endcase
     endcase
 endmodule
